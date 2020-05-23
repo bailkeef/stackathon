ad5652d10cdd016866cb780454851efb7e7872aa
Bailey Keefer <bnk423@gmail.com>
2020-05-22 22:24:05 -0400

    fixes to deploy

--- twilio/send-sms.js
+++ twilio/send-sms.js
@@ -1,7 +1,10 @@
 const accountSid = process.env.TWILIO_ACCOUNT_SID
 const authToken = process.env.TWILIO_AUTH_TOKEN
 
-const client = require('twilio')(accountSid, authToken)
+const client = require('twilio')(
+  'AC06f15fd6084ca5770dcf3eeea350159a',
+  'ad0d92f6921d5170a72b186985ddac77'
+)
 
 function sendText(phoneNumber, message) {
   console.log('im in here now')
